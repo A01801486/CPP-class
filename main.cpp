b15564d1742d318444186f0d71775db3087afb28
A01801486 <149412157+A01801486@users.noreply.github.com>
2023-11-01 16:26:47 +0000

    clase 1

---
+++ main.cpp
@@ -0,0 +1,53 @@
+# include <iostream>
+# include <cmath>
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+// int, char(carácter), string, nool, float, double, cast
+
+int main ()
+{
+    float x = 0.0;
+    cout <<  "La variable x es: " << x << endl;
+    //int z = int(x);
+    int z = sqrt(4);
+
+    cin >> z;
+
+    if (z == 2 && x != 0)                     // AND -> &&    OR -> ||    NOT -> != 
+    {
+        cout << "z es 2" << endl;
+    }
+    else
+    {
+        cout << "z no es 2" << endl;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+
+    int i = 0;
+    while (i<10)
+    {
+        cout << "Dentro del while" << i << endl;
+        i = i + 2;
+    }
+
+    i = 0; /*<---No se pone el int, porque ya se había declarado antes*/
+    do
+    {
+        cout << "Dentro del while" << i << endl;
+        i = i + 3;
+    } while (i < 10);
+    
+
+
+
+
+/*do while ejecuta las instrucciones y después revisa la condición
+    for i in range (0,10,2) -> [start,stop,step]*/
+}
